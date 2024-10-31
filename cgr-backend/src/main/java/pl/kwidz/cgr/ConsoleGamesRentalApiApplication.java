76c71f78d381b43047d14b6a01d31ac2b0ea0466
PawelKwidzinski <solvap83@gmail.com>
2024-10-31 11:53:52 +0100

    Initial commit

---
+++ cgr-backend/src/main/java/pl/kwidz/cgr/ConsoleGamesRentalApiApplication.java
@@ -0,0 +1,13 @@
+package pl.kwidz.cgr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsoleGamesRentalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsoleGamesRentalApiApplication.class, args);
+	}
+
+}
---
+++ cgr-backend/src/test/java/pl/kwidz/cgr/ConsoleGamesRentalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kwidz.cgr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsoleGamesRentalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
