e61be1df7cea3b909fbf7c4911d89d0004bf7675
PawelKwidzinski <solvap83@gmail.com>
2024-11-07 11:47:35 +0100

    Add the application auditor aware:: Auditing

--- cgr-backend/src/main/java/pl/kwidz/cgr/ConsoleGamesRentalApiApplication.java
+++ cgr-backend/src/main/java/pl/kwidz/cgr/ConsoleGamesRentalApiApplication.java
@@ -10,7 +10,7 @@ import pl.kwidz.cgr.role.Role;
 import pl.kwidz.cgr.role.RoleRepository;
 
 @SpringBootApplication
-@EnableJpaAuditing
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 @EnableAsync
 public class ConsoleGamesRentalApiApplication {
 
