121da1867abd7145265583bc215b8a57b3825a5e
PawelKwidzinski <solvap83@gmail.com>
2024-11-15 09:18:11 +0100

    Add activate account and connect it with backend Ref. #23

--- cgr-frontend/src/app/app-routing.module.ts
+++ cgr-frontend/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {LoginComponent} from './pages/login/login.component';
 import {RegisterComponent} from './pages/register/register.component';
+import {ActivateAccountComponent} from './pages/activate-acconunt/activate-account.component';
 
 const routes: Routes = [{
   path: 'login',
@@ -10,6 +11,10 @@ const routes: Routes = [{
   {
     path: 'register',
     component: RegisterComponent
+  },
+  {
+    path: 'activate-account',
+    component: ActivateAccountComponent
   }
 
 ];
