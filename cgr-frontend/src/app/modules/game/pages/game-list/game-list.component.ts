b97c7609fb6369ee715d3fb40a01aaed3442ffb2
PawelKwidzinski <solvap83@gmail.com>
2024-11-19 10:17:58 +0100

    Add pagination in game-list component Ref. #33

--- cgr-frontend/src/app/modules/game/pages/game-list/game-list.component.ts
+++ cgr-frontend/src/app/modules/game/pages/game-list/game-list.component.ts
@@ -11,7 +11,8 @@ import {PageResponseGameResponse} from '../../../../services/models/page-respons
 export class GameListComponent implements OnInit {
   gameResponse: PageResponseGameResponse = {};
   page = 0;
-  size = 5;
+  size = 2;
+  pages: any = [];
 
   constructor(private gameService: GameService, private router: Router) {
   }
@@ -27,8 +28,40 @@ export class GameListComponent implements OnInit {
       }).subscribe({
       next: (games) => {
         this.gameResponse = games;
+        this.pages = Array(this.gameResponse.totalPages)
+          .fill(0)
+          .map((x, i) => i)
       }
     });
+  }
+
+  gotToPage(page: number) {
+    this.page = page;
+    this.findAllGames();
+  }
+
+  goToFirstPage() {
+    this.page = 0;
+    this.findAllGames();
+  }
+
+  goToPreviousPage() {
+    this.page --;
+    this.findAllGames();
+  }
+
+  goToLastPage() {
+    this.page = this.gameResponse.totalPages as number - 1;
+    this.findAllGames();
+  }
+
+  goToNextPage() {
+    this.page++;
+    this.findAllGames();
+  }
+
+  get isLastPage() {
+    return this.page === this.gameResponse.totalPages as number - 1;
   }
 
 }
